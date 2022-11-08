9c75a9035dd91413fe08f5be7a39ff9342645d14
gabdykalyk <gabdykalyk@gmail.com>
2022-11-08 12:25:08 +0600

    feat: added NotificationComponent

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import Header from '../../components/Header/Header';
+import NotificationComponent from '../../components/Notification/NotificationComponent';
 import SearchComponent from '../../components/SearchComponent/SearchComponent';
 
 export default function MainPage() {
@@ -6,6 +7,7 @@ export default function MainPage() {
         <div>
             <Header />
             <SearchComponent />
+            <NotificationComponent />
         </div>
     )
 }
